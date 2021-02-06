e4934c26219745cf59c5fd7024fa6b8b09228bb8
Šimon Brandner <simon.bra.ag@gmail.com>
2021-02-06 09:54:00 +0100

    Added keyboard enums
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/Keyboard.ts
+++ src/Keyboard.ts
@@ -16,6 +16,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import { _td } from "./languageHandler";
+
 export enum Key {
     HOME = "Home",
     END = "End",
@@ -73,7 +75,32 @@ export enum Key {
     Z = "z",
 }
 
+// TS: once languageHandler is TS we can probably inline this into the enum
+_td("Alt");
+_td("Alt Gr");
+_td("Shift");
+_td("Super");
+_td("Ctrl");
+
+export enum Modifiers {
+    ALT = "Alt", // Option on Mac and displayed as an Icon
+    ALT_GR = "Alt Gr",
+    SHIFT = "SHIFT",
+    SUPER = "Super", // should this be "Windows"?
+    // Instead of using below, consider CMD_OR_CTRL
+    COMMAND = "Command", // This gets displayed as an Icon
+    CONTROL = "Ctrl",
+}
+
 export const isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+
+// Meta-modifier: isMac ? CMD : CONTROL
+export const CMD_OR_CTRL = isMac ? Modifiers.COMMAND : Modifiers.CONTROL;
+
+export interface IKeybind {
+    modifiers?: Modifiers[];
+    key: Key;
+}
 
 export function isOnlyCtrlOrCmdKeyEvent(ev) {
     if (isMac) {
