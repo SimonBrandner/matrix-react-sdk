571fa345d9c09cad16d9c2fa80c9362fd67a4a38
Šimon Brandner <simon.bra.ag@gmail.com>
2021-02-07 10:14:04 +0100

    Added a component for displaying kayboard shortcut
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

---
+++ src/components/views/elements/KeyboardShortcut.tsx
@@ -0,0 +1,76 @@
+/*
+Copyright 2020 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import * as React from "react";
+import { _td } from "../../../languageHandler";
+import {IKeybind, isMac, Key, Modifiers} from "../../../Keyboard";
+
+const alternateKeyName: Record<string, string> = {
+    [Key.PAGE_UP]: _td("Page Up"),
+    [Key.PAGE_DOWN]: _td("Page Down"),
+    [Key.ESCAPE]: _td("Esc"),
+    [Key.ENTER]: _td("Enter"),
+    [Key.SPACE]: _td("Space"),
+    [Key.HOME]: _td("Home"),
+    [Key.END]: _td("End"),
+};
+
+const alternateModifierName: Record<string, string> = {
+    [Modifiers.ALT]: _td("Alt"),
+    [Modifiers.ALT_GR]: _td("Alt Gr"),
+    [Modifiers.SHIFT]: _td("Shift"),
+    [Modifiers.SUPER]: _td("Super"),
+    [Modifiers.CONTROL]: _td("Ctrl"),
+}
+
+const keyIcon: Record<string, string> = {
+    [Key.ARROW_UP]: "↑",
+    [Key.ARROW_DOWN]: "↓",
+    [Key.ARROW_LEFT]: "←",
+    [Key.ARROW_RIGHT]: "→",
+};
+
+const modifierIcon: Record<string, string> = {
+    [Modifiers.COMMAND]: "⌘",
+};
+
+if (isMac) {
+    modifierIcon[Modifiers.ALT] = "⌥";
+}
+
+interface IProps {
+    keybind: IKeybind;
+}
+
+export default class Shortcut extends React.Component<IProps> {
+    render() {
+        const key = this.props.keybind.key;
+        const modifiers = this.props.keybind.modifiers.map((modifier) => {
+            return (
+                <React.Fragment key={modifier}>
+                    <kbd>{ modifierIcon[modifier] || alternateModifierName[modifier] || modifier }</kbd>+
+                </React.Fragment>
+            );
+        });
+
+        return (
+            <div className="mx_KeyboardShortcut">
+                {modifiers}
+                <kbd>{ keyIcon[key] || alternateKeyName[key] || key }</kbd>
+            </div>
+        );
+    }
+}
